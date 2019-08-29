eafeb17adcaf1332bd6cfa267ba5f95371357f52
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-08-29 23:29:20 +0200

    First webpack's configuration

---
+++ app
@@ -0,0 +1,3 @@
+'use strict';
+const world = require('./world');
+console.log('hello ' + world);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const world = require('./world');
+console.log('hello ' + world);
+
+const path = require('path');
